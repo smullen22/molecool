df471e47be2db1ebf4af47416761604c65398bfe
Sarah Mullen <sarahmullen@vt.edu>
2023-03-11 13:29:33 -0500

    test1

--- molecool/functions.py
+++ molecool/functions.py
@@ -7,10 +7,6 @@ Handles the primary functions
 
 import os
 import numpy as np
-import matplotlib.pyplot as plt
-
-from mpl_toolkits.mplot3d import Axes3D
-
 
 def canvas(with_attribution=True):
     """
