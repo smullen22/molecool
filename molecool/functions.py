c0d21820f9a00c22b3b3ba92ee3f4bdb085a2e4a
Sarah Mullen <sarahmullen@vt.edu>
2023-03-11 13:28:02 -0500

    test

--- molecool/functions.py
+++ molecool/functions.py
@@ -57,11 +57,6 @@ def zen():
     
     return quote
 
-def calculate_distance(rA, rB):
-    d=(rA-rB)
-    dist=np.linalg.norm(d)
-    return dist
-
 def open_pdb(f_loc):
     with open(f_loc) as f:
         data = f.readlines()
@@ -74,29 +69,6 @@ def open_pdb(f_loc):
             c.append(c2)
     coords = np.array(c)
     return sym, coords
-
-def open_xyz(file_location):
-    
-    # Open an xyz file and return symbols and coordinates.
-    xyz_file = np.genfromtxt(fname=file_location, skip_header=2, dtype='unicode')
-    symbols = xyz_file[:,0]
-    coords = (xyz_file[:,1:])
-    coords = coords.astype(np.float)
-    return symbols, coords
-
-def write_xyz(file_location, symbols, coordinates):
-    
-    num_atoms = len(symbols)
-    
-    with open(file_location, 'w+') as f:
-        f.write('{}\n'.format(num_atoms))
-        f.write('XYZ file\n')
-        
-        for i in range(num_atoms):
-            f.write('{}\t{}\t{}\t{}\n'.format(symbols[i], 
-                                              coordinates[i,0], coordinates[i,1], coordinates[i,2]))
-
-
 
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
